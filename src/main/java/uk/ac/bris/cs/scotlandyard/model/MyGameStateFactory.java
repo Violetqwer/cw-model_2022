cd36d15396cf5dfc8391ebc065bae613ebe4d9a4
Violet Chen <yb21068@bristol.ac.uk>
2023-04-24 15:17:15 +0100

    update

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -6,13 +6,25 @@ import com.google.common.collect.ImmutableList;
 import javax.annotation.Nonnull;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.*;
+
+
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Piece;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
 
-import java.util.ArrayList;
-import java.util.Optional;
+
+
+import java.util.*;
 import java.util.stream.Stream;
+import java.util.HashSet;
+import java.util.Set;
+import com.google.common.collect.Sets;
+
+import com.google.common.graph.*;
 
 /**
  * cw-model
@@ -47,7 +59,8 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 		@Override
 		public int getCount(@Nonnull Ticket ticket) {
-			return 0;
+
+			return player.tickets().get(ticket);
 		}
 	}
 
@@ -60,8 +73,8 @@ public final class MyGameStateFactory implements Factory<GameState> {
 	//请注意，构造函数是私有的，因为只有封闭类中的构建器（以及后来的 advance 方法）才会调用它。
 	// 下面的@Nonnull
 	//		@Override的创建都属于step2
-	private final class MyGameState implements GameState{
-		private  final GameSetup setup;
+	private final class MyGameState implements GameState {
+		private final GameSetup setup;
 		private final Player mrX;
 		private final ImmutableList<Player> detectives;
 		private final ImmutableList<LogEntry> log;
@@ -72,6 +85,15 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 		//Step 3:CONSTRUCTOR 构造函数
 		public MyGameState(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
+
+			//step 4的初始化检查: 添加参数的非空检查
+			if (setup == null || mrX == null || detectives == null) {
+				throw new IllegalArgumentException("setup, mrX, and detectives cannot be null.");
+			}
+			if (setup.moves.isEmpty()) {
+				throw new IllegalArgumentException("move is empty!");
+			}
+
 			this.setup = setup;
 			this.mrX = mrX;
 			this.detectives = detectives;
@@ -87,14 +109,13 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			//初始化ticketBoards
 			ArrayList<TicketBoard> boards = new ArrayList<>();
 			boards.add(new TicketBoardImpl(mrX, setup));
-			for (Player detective : detectives){
+			for (Player detective : detectives) {
 				boards.add(new TicketBoardImpl(detective, setup));
 			}
 			this.ticketBoards = ImmutableSet.copyOf(boards);
 
 
 		}
-
 
 
 		@Nonnull
@@ -106,37 +127,96 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		@Nonnull
 		@Override
 		public ImmutableSet<Piece> getPlayers() {
-			return null;
+			//创建一个包含所有玩家的集合
+			ImmutableSet.Builder<Piece> players = ImmutableSet.builder();
+
+			//add Mr.X
+			players.add(mrX.piece());
+
+			//add detective
+			for(Player detective : detectives){
+				players.add(detective.piece());
+			}
+
+			//返回构建的不可变集合
+			return players.build();
 		}
 
+
+		//step 5
+		//根据注释的意思，需要遍历所有的警探（detective），
+		// 如果某个警探的棋子（Piece）和输入的参数（Piece.Detective detective）相同，
+		// 则返回该警探的位置，否则返回 Optional.empty()。
 		@Nonnull
 		@Override
 		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			//用for循环所有detectives
+			for (Player player : detectives) {
+				//如果当前detectives的pieces 与指定的棋子类型相同
+				if (player.piece() == detective) {
+					//return 该警探位置
+					return Optional.of(player.location());
+				}
+			}
+			//如果没有匹配，返回Optional.empty（）
 			return Optional.empty();
 		}
 
 		@Nonnull
 		@Override
 		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
-			return Optional.empty();
+
+			return ticketBoards.stream().filter(ticketBoard -> ticketBoard.piece() == piece).findFirst();
 		}
 
 		@Nonnull
 		@Override
 		public ImmutableList<LogEntry> getMrXTravelLog() {
-			return null;
+
+			return log;
 		}
 
 		@Nonnull
 		@Override
 		public ImmutableSet<Piece> getWinner() {
+			//check游戏状态，确定哪一方胜利
+			if (/*MrX获胜*/){
+				return ImmutableSet.of(Piece.MrX);
+
+			}else if (/*detective获胜*/){
+				//创建一个包含所有侦探的集合
+				ImmutableSet.Builder<Piece> winners = ImmutableSet.builder();
+
+				//add all detectives
+				for (Player detective : detectives){
+					winners.add(detective.piece());
+				}
+
+				return winners.build();
+			}else{
+				//游戏尚未结束，返回empty不可变集合
+				return ImmutableSet.of();
+			}
+
 			return null;
 		}
 
 		@Nonnull
 		@Override
 		public ImmutableSet<Move> getAvailableMoves() {
-			return null;
+			//创建一个ImmutableSet.Builder对象，用于构建最终返回的不可变集合
+			ImmutableSet.Builder<Move> moves = ImmutableSet.builder();
+
+			//将mr.X 的singlemove加入moveslist
+			moves.addAll(makeSingleMoves(mrX));
+
+			//循环所有侦探，将每个侦探的单步移动加入moves集合
+			for (Player player : detectives) {
+				moves.addAll(makeSingleMoves(player));
+			}
+
+			//create 不可变集合并返回
+			return moves.build();
 		}
 
 		@Nonnull
@@ -145,8 +225,173 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			return null;
 		}
 
+
+		//创建SingleMove的构造方法
+		public static class SingleMove implements Move {
+			private final Piece piece;
+			private final Ticket ticket;
+			private final int destination;
+
+			public SingleMove(Piece piece, Ticket ticket, int destination){
+				this.piece = piece;
+				this.ticket = ticket;
+				this.destination = destination;
+			}
+
+			@Override
+			public Piece piece(){
+				return piece;
+			}
+
+			@Override
+			public Ticket ticket(){
+				return ticket;
+
+			}
+
+			@Override
+			public int destination(){
+				return destination;
+			}
+
+			@Override
+			public boolean isDoubleMove(){
+				return false;
+			}
+
+		}
+
+
+
+
+
+
+
+
+		//step 6 AVAILABLE MOVES
+
+		//1.创建三个private方法
+		//isDestinationOccupied：检查给定的节点是否已经被占据
+		private boolean isDestinationOccupied(int destinationNode){
+			for (Player detective : detectives){
+				if (detective.location() == destinationNode){
+					return true;
+				}
+			}
+			return false;
+		}
+
+		//
+		private ScotlandYard.Ticket fromTransport(ScotlandYard.Transport transport){
+			switch (transport){
+				case TAXI:
+					return Ticket.TAXI;
+				case BUS:
+					return Ticket.BUS;
+				case UNDERGROUND:
+					return Ticket.UNDERGROUND;
+				default:
+					throw new IllegalArgumentException("Invalid transport type UWU");
+			}
+		}
+
+		private boolean hasTickets(Player player, Ticket ticket){
+			return player.tickets().get(ticket) > 0;
+		}
+
+
+
+
+
+
+
+		// 创建一个HashSet来储存单步移动对象
+		private Set<Move> makeSingleMoves(Player player) {
+			HashSet<Move> moves = new HashSet<>();
+
+			// 获取万千当前节点
+			int currentNode = player.location();
+
+			// 遍历当前节点所有相邻节点
+			for (Integer destination : setup.graph.adjacentNodes(currentNode)) {
+
+				//获取当前节点与目标节点之间的边的 Transport 类型
+				Set<ScotlandYard.Transport> transports = setup.graph.edgeValueOrDefault(currentNode, destination, ImmutableSet.of());
+
+				// 检查目标节点是否可用
+				if (!isDestinationOccupied(destination)) {
+
+					// 循环支持的所有交通
+					for (ScotlandYard.Transport transport : transports) {
+
+						// 将Transport转换成对应票的类型
+						Ticket ticket = fromTransport(transport);
+
+						// Check if the player has the corresponding ticket
+						if (player.tickets().get(ticket) > 0) {
+
+							// If so, create a new single move and add it to the moves set
+							Move.SingleMove newMove = new Move.SingleMove(player.piece(),ticket,destination);
+							moves.add(newMove);
+
+
+						}
+					}
+				}
+			}
+
+			// Check if Mr.X has any double move tickets available
+			if (player.piece() == Piece.MrX && player.has(Ticket.DOUBLE)) {
+				moves.addAll(makeDoubleMoves(player, moves));
+			}
+
+			return moves;
+		}
+
+		private Set<Move.DoubleMove> makeDoubleMoves(Player player, Set<Move.SingleMove> singleMoves) {
+			HashSet<Move.DoubleMove> doubleMoves = new HashSet<>();
+
+			for (Move.SingleMove firstMove : singleMoves) {
+
+				// Get the intermediate node from the first single move
+				int intermediateNode = firstMove.destination;
+
+				// Loop through all edges from the intermediate node
+				for (Edge<Integer, ScotlandYard.Transport> edge : setup.graph.getEdgesFrom(intermediateNode)) {
+
+					// Get the destination node of the edge
+					int destinationNode = edge.destination.value();
+
+					// Check if the destination node is available
+					if (!isDestinationOccupied(destinationNode)) {
+
+						// Loop through all transport types supported by the edge
+						for (ScotlandYard.Transport transport : edge.data()) {
+
+							// Convert the transport to the corresponding ticket type
+							Ticket ticket = Ticket.fromTransport(transport);
+
+							// Check if the player has the corresponding ticket
+							if (player.has(ticket) && player.has(Ticket.DOUBLE)) {
+
+								// If so, create a new double move and add it to the doubleMoves set
+								Move.DoubleMove newDoubleMove = new Move.DoubleMove(player.piece(),
+										firstMove.ticket(), firstMove.destination(),
+										ticket, destinationNode);
+								doubleMoves.add(newDoubleMove);
+							}
+						}
+					}
+				}
+			}
+
+			return doubleMoves;
+		}
 	}
 }
+
+
+
 
 
 
